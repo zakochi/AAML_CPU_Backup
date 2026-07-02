4249133b92eeeb326b5dd6b47d136fa7b1eef1f5
ざこちぃ <abcd5050500@gmail.com>
2026-07-02 22:51:22 +0800

    Update build_SoC.tcl

--- Platform/hw/build_SoC.tcl
+++ Platform/hw/build_SoC.tcl
@@ -5,7 +5,7 @@ set proj_name "SoC"
 
 create_project ${proj_name} ./${proj_name} -part xc7a100tcsg324-1 -force
 
-# 1. 載入硬體原始碼
+# Source files
 set v_files [glob -nocomplain "$hw_srcs/sources_1/imports/*.v"]
 if {[llength $v_files] > 0} { add_files -norecurse $v_files }
 
@@ -16,7 +16,7 @@ add_files -norecurse [file normalize "$hw_srcs/sources_1/bd/MMIO/MMIO.bd"]
 add_files -norecurse [file normalize "$hw_srcs/sources_1/imports/SoC.v"]
 add_files -fileset constrs_1 [file normalize "$hw_srcs/constrs_1/new/Nexys-A7-100T-Master.xdc"]
 
-# 2. 處理 riscv_defs.v
+# riscv_defs.v
 set defs_file [file normalize "$hw_srcs/sources_1/riscv_defs.v"]
 if {[file exists $defs_file]} {
     add_files -norecurse $defs_file
@@ -24,7 +24,6 @@ if {[file exists $defs_file]} {
     set_property is_global_include true [get_files $defs_file]
 }
 
-# 3. 【這就是你要的流程】把軟體編好的 boot.hex 加入專案
 set hex_path [file normalize "$root_dir/build/boot/boot.hex"]
 if {[file exists $hex_path]} {
     add_files -norecurse $hex_path
@@ -35,14 +34,14 @@ if {[file exists $hex_path]} {
 update_compile_order -fileset sources_1
 open_bd_design [get_files MMIO.bd]
 
-# 4. 拔除幽靈 ELF，防止干擾
+
 set elf_obj [get_files -quiet "*/bootloader.elf"]
 if {$elf_obj != ""} {
     set_property SCOPED_TO_REF {} $elf_obj
     set_property SCOPED_TO_CELLS {} $elf_obj
 }
 
-# 5. 生成與合成
+
 assign_bd_address
 validate_bd_design
 save_bd_design
@@ -53,7 +52,6 @@ set_property top SoC [current_fileset]
 launch_runs impl_1 -to_step write_bitstream -jobs 8
 wait_on_run impl_1
 
-# 6. 複製結果
 set bit_src [file normalize "./${proj_name}/${proj_name}.runs/impl_1/SoC.bit"]
 if {[file exists $bit_src]} {
     file mkdir "$root_dir/build"
